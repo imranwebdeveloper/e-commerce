b706ed2d272c8ff95f705899fe9d6c570c9f5919
Nurul Islam <islam.nurul@cg-bd.com>
2022-01-30 15:06:35 +0600

    Basic Setup

---
+++ src/pages/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = () => {
+  return <div className="text-center text-4xl">Details page</div>;
+};
+
+export default Details;
---
+++ src/pages/Product.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Product = () => {
+  return <div className="text-4xl">Product</div>;
+};
+export default Product;
